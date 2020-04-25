04a0822aa76164676e37733eb20cabc6c09015b9
Ildar Bikmamatov <ildar@bayrell.org>
2020-04-25 16:15:05 +0100

    docker swarm ready

--- example/docker.compose.router.sh
+++ example/docker.compose.router.sh
@@ -7,10 +7,10 @@ docker volume create router_data
 sleep 5
 
 docker run -d --name router --hostname="router.local" --restart=always --log-driver=journald \
-	-e DNS_RESOLVER=8.8.8.8 -p 8080:80 -p 8443:443 -v router_data:/data -env-file=common.env \
+	-e DNS_RESOLVER=127.0.0.11 -p 8080:8080 -p 80:80 -p 443:443 -v router_data:/data -env-file=common.env \
 	--network="dev_backend" --env CONTROL_PANEL=cloud_web_panel \
 	bayrell/cloud_router:latest
 
 sleep 5	
-	
+
 docker ps | grep router
--- example/docker.compose.service.sh
+++ example/docker.compose.service.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+docker service rm dev_cloud_web_panel
 docker stack deploy -c service.yaml dev --with-registry-auth
