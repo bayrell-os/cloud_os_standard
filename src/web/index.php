669ed715746ad550fb21348834a34dd1d2430638
Ildar Bikmamatov <ildar@bayrell.org>
2020-04-26 00:32:21 +0100

    load keys from secrets

--- src/web/index.php
+++ src/web/index.php
@@ -1,29 +1,5 @@
 <?php
 
-set_time_limit(30);
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
-ini_set("display_startup_errors", 1);
-ini_set("track_errors", 1);
-ini_set("html_errors", 1);
 $basepath = dirname(__DIR__);
-
-include "../lib/Runtime/php/loader.php";
-$app = new Loader();
-
-try
-{
-	$app
-		->main_module( "App" )
-		->base_path( $basepath )
-		->add_src( $basepath . "/app" )
-		->add_src( $basepath . "/lib" )
-		->run_web_request()
-	;
-}
-catch (\Exception $ex)
-{
-	$app->exception($ex);
-}
-
-exit($app->return_code);
+include $basepath . "/app/App/php/web.php";
+run_web ($basepath);
