e8a0d59cab527dd5eb780b168b4a225dbeaf3612
Ildar Bikmamatov <ildar@bayrell.org>
2020-04-29 10:52:43 +0100

    task sync_services

--- src/app/App/app.php
+++ src/app/App/app.php
@@ -2,19 +2,36 @@
 
 set_time_limit(30);
 error_reporting(E_ALL);
-ini_set("display_errors", 1);
-ini_set("display_startup_errors", 1);
-ini_set("track_errors", 1);
-ini_set("html_errors", 1);
+if (php_sapi_name() != "cli")
+{
+	ini_set("display_errors", 1);
+	ini_set("display_startup_errors", 1);
+	ini_set("track_errors", 1);
+}
+else
+{
+	ini_set("display_errors", 0);
+	ini_set("display_startup_errors", 0);
+	ini_set("track_errors", 1);
+}
+
+
+//ini_set("html_errors", 1);
 
 $basepath = dirname(dirname(__DIR__));
 include $basepath . "/lib/Runtime/php/loader.php";
 
 $app = new Loader();
-$env = [
-	"AUTH_PRIVATE_KEY" => file_get_contents("/run/secrets/AUTH_PRIVATE_KEY"),
-	"AUTH_PUBLIC_KEY" => file_get_contents("/run/secrets/AUTH_PUBLIC_KEY"),
-];
+$env = getenv();
+$env = array_merge
+(
+	$env,
+	[
+		"AUTH_PRIVATE_KEY" => file_get_contents("/run/secrets/AUTH_PRIVATE_KEY"),
+		"AUTH_PUBLIC_KEY" => file_get_contents("/run/secrets/AUTH_PUBLIC_KEY"),
+		"MYSQL_DB" => "web_panel",
+	]
+);
 
 if (!isset($argv))
 {
