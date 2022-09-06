5f3975928f76b13bb2ec3cb048fe37645a49a0db
Ildar Bikmamatov <ildar@bayrell.org>
2022-09-06 19:23:54 +0600

    add basic auth

--- files/root/run.d/50-cloud.yaml.sh
+++ files/root/run.d/50-cloud.yaml.sh
@@ -1,11 +1,4 @@
 if [ ! -d /data/yaml ]; then
 	cp -R /root/yaml /data
-	
-	ENV_PATH=/data/yaml/cloud_os/env.conf
-	cat /data/yaml/cloud_os/env.example > $ENV_PATH
-	sed -i "s|CLOUD_OS_KEY=.*|CLOUD_OS_KEY=${CLOUD_OS_KEY}|g" $ENV_PATH
-	sed -i "s|CLOUD_OS_GATEWAY=.*|CLOUD_OS_GATEWAY=${CLOUD_OS_GATEWAY}|g" $ENV_PATH
-	
-	# echo $CLOUD_OS_KEY > /data/yaml/cloud_os/cloud.key
 	chown -R www-data:www-data /data/yaml
 fi
--- files/root/run.d/50-ssh.server.sh
+++ files/root/run.d/50-ssh.server.sh
@@ -12,20 +12,23 @@ fi
 
 if [ ! -z $SSH_USER ] && [ ! -z $SSH_PASSWORD ]; then
 	
+	SSH_PASSWORD_HASH=`echo $SSH_PASSWORD | openssl passwd -1 -stdin`
 	sed -i "s|AllowUsers .*|AllowUsers $SSH_USER|g" /data/ssh/sshd_config
 	
 	echo "Create user $SSH_USER"
 	if [ ! -z $WWW_UID ] && [ ! -z $WWW_GID ]; then
 	
 		echo "$SSH_USER:x:$WWW_UID:$WWW_GID:www-data:/data/home:/bin/bash" >> /etc/passwd
-		usermod --password $(echo $SSH_PASSWORD | openssl passwd -1 -stdin) $SSH_USER
+		usermod --password $SSH_PASSWORD_HASH $SSH_USER
 	
 	else
 	
 		echo "$SSH_USER:x:33:33:www-data:/data/home:/bin/bash" >> /etc/passwd
-		usermod --password $(echo $SSH_PASSWORD | openssl passwd -1 -stdin) $SSH_USER
+		usermod --password $SSH_PASSWORD_HASH $SSH_USER
 	
 	fi
+	
+	echo "$SSH_USER:$SSH_PASSWORD_HASH" >> /etc/nginx/inc/htpasswd.inc
 	
 else
 	echo "Disable ssh server"
