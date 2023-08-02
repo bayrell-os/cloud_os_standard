779a2769a3c890edf361800fa60c1627e4117dfb
Ildar Bikmamatov <ildar@bayrell.org>
2023-08-02 18:46:50 +0600

    add index page

--- src/html/index.php
+++ src/html/index.php
@@ -5,23 +5,17 @@ ini_set('display_errors', 'on');
 ini_set('html_errors', 'on');
 set_time_limit(30);
 
-// Include vendor
-// require ROOT_PATH . "/vendor/autoload.php";
+/* Create context */
+$context = \Runtime\rtl::createContext([
+	"entry_point" => "Bayrell.CloudOS.Main",
+	"modules" => [
+		"Bayrell.CloudOS"
+	],
+]);
 
-/* Include loader */
-include ROOT_PATH . "/lib/Runtime/php/Loader.php";
+/* Start context */
+$context->start($context);
 
-/* Include enviroments */
-$env = include ROOT_PATH . "/env.php";
-
-/* Run app */
-$loader = ( new \Runtime\Loader() )
-	->addIncludePath( ROOT_PATH . "/app" )
-	->addIncludePath( ROOT_PATH . "/lib" )
-	->setEnv($env)
-	->setMainModule("Bayrell.CloudOS")
-	->setMainClass("Bayrell.CloudOS.Backend")
-	->setEntryPoint("Runtime.Web.App.CGI")
-	->run();
-
-exit($loader->exit_code);
+/* Run web app */
+$context = \Runtime\rtl::getContext();
+$context->run($context);
