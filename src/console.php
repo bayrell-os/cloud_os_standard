f42cb49b1a7df85bb3ba9106e0cc1fd4bd10b0f4
Ildar Bikmamatov <ildar@bayrell.org>
2023-10-05 01:18:51 +0600

    remove old files

--- src/console.php
+++ src/console.php
@@ -5,18 +5,23 @@ ini_set('display_errors', 'on');
 ini_set('html_errors', 'on');
 set_time_limit(-1);
 
-/* Setup entry point */
-define('ENTRY_POINT', 'Bayrell.CloudOS.Console.Main');
-
 /* Init context */
 require_once __DIR__ . "/init.php";
 
 /* Run console app */
-$exit_code = \Runtime\rtl::runApp('Bayrell.CloudOS.Console.Main', [
-    'Bayrell.CloudOS',
-    'Bayrell.CloudOS.Console',
-],
-\Runtime\Dict::from([
-    'cli_args' => \Runtime\Collection::from($argv),
-]));
+$exit_code = \Runtime\rtl::runApp(
+    
+    /* Entry point */
+    'Bayrell.CloudOS.Console.Main',
+    
+    /* Modules */
+    [
+        'Bayrell.CloudOS.Console',
+    ],
+    
+    /* Context parameters */
+    \Runtime\Dict::from([
+        'cli_args' => \Runtime\Collection::from($argv),
+    ])
+);
 exit($exit_code);
--- src/public/index.php
+++ src/public/index.php
@@ -4,13 +4,17 @@ ini_set('display_errors', 'on');
 ini_set('html_errors', 'on');
 set_time_limit(30);
 
-/* Setup entry point */
-define('ENTRY_POINT', 'Bayrell.CloudOS.Main');
-
 /* Init context */
 require_once dirname(__DIR__) . "/init.php";
 
 /* Run web app */
-\Runtime\rtl::runApp('Bayrell.CloudOS.Main', [
-    'Bayrell.CloudOS'
-]);
+\Runtime\rtl::runApp(
+    
+    /* Entry point */
+    'Bayrell.CloudOS.Main',
+    
+    /* Modules */
+    [
+        'Bayrell.CloudOS'
+    ]
+);
