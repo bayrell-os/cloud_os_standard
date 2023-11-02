879e0f427027a79f5ce109e9d65ebc08059d57c8
Ildar Bikmamatov <ildar@bayrell.org>
2023-11-02 21:16:41 +0600

    stop service

--- files/root/loop_cron.sh
+++ files/root/loop_cron.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-exit 0
-
 while true
 do
     sleep 15
-    #/srv/console.php docker:services:update --quiet
+    /srv/console.php docker:services:update --quiet
     #/srv/console.php docker:nginx:update --quiet
     #/srv/console.php admin:update --quiet
 done
