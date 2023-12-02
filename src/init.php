e5916636fab6114b51750b0d4fde9581a77a5829
Ildar Bikmamatov <ildar@bayrell.org>
2023-12-02 20:03:17 +0600

    init ssl table model

--- src/init.php
+++ src/init.php
@@ -15,6 +15,7 @@ $loader->addPsr4("Runtime\\Unit\\",  __DIR__ . "/lib/Runtime.Unit/php");
 $loader->addPsr4("Runtime\\Web\\",  __DIR__ . "/lib/Runtime.Web/php");
 $loader->addPsr4("Runtime\\Widget\\",  __DIR__ . "/lib/Runtime.Widget/php");
 $loader->addPsr4("Runtime\\Widget\\Crud\\",  __DIR__ . "/lib/Runtime.Widget.Crud/php");
+$loader->addPsr4("Runtime\\Widget\\Dialog\\",  __DIR__ . "/lib/Runtime.Widget.Dialog/php");
 $loader->addPsr4("Runtime\\Widget\\Table\\",  __DIR__ . "/lib/Runtime.Widget.Table/php");
 $loader->addPsr4("Runtime\\XML\\",  __DIR__ . "/lib/Runtime.XML/php");
 
