d750393ddf0f6dfd189de682162a1e303c3a1567
Ildar Bikmamatov <ildar@bayrell.org>
2024-01-14 00:01:11 +0600

    app variables fix

--- files/root/cloud_os_data.sql
+++ files/root/cloud_os_data.sql
@@ -440,5 +440,4 @@ INSERT INTO "app_modificators" ("app_id", "modificator_id") VALUES (1,	3);
 INSERT INTO "app_modificators" ("app_id", "modificator_id") VALUES (2,	1);
 INSERT INTO "app_modificators" ("app_id", "modificator_id") VALUES (2,	2);
 INSERT INTO "app_modificators" ("app_id", "modificator_id") VALUES (2,	3);
-
 -- 
