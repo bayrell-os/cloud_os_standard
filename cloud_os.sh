f436620814660563ccec9a9352a4751c80bf7bdc
Ildar Bikmamatov <ildar@bayrell.org>
2024-08-26 21:16:25 +0600

    compose fix

--- cloud_os.sh
+++ cloud_os.sh
@@ -5,7 +5,7 @@ SCRIPT=$(readlink -f $0)
 SCRIPT_PATH=`dirname $SCRIPT`
 
 RETVAL=0
-VERSION=0.5.1-amd64
+VERSION=0.5.1
 TAG=`date '+%Y%m%d_%H%M%S'`
 ENV_CONFIG_PATH=$SCRIPT_PATH/example/env.conf
 
@@ -25,27 +25,28 @@ function read_env_config()
 
 function generate_env_config()
 {
-	if [ ! -f "$ENV_CONFIG_PATH" ]; then
-		cat $SCRIPT_PATH/example/env.example > $ENV_CONFIG_PATH
-	fi
-	
-	read_env_config
-	
 	if [ -z "$CLOUD_OS_KEY" ]; then
 		CLOUD_OS_KEY=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 128`
-		echo "CLOUD_OS_KEY=${CLOUD_OS_KEY}" >> $ENV_CONFIG_PATH
 	fi
 	
 	if [ -z "$SSH_PASSWORD" ]; then
 		SSH_PASSWORD=`cat /dev/urandom | tr -dc 'a-zA-Z0-9!@%^*_\-+~' | head -c 16`
-		echo "SSH_PASSWORD=${SSH_PASSWORD}" >> $ENV_CONFIG_PATH
 	fi
 	
 	if [ -z "$SSH_USER" ]; then
 		echo "Enter ssh username for Cloud OS:"
 		read SSH_USER
-		echo "SSH_USER=${SSH_USER}" >> $ENV_CONFIG_PATH
 	fi
+	
+	local text=""
+	text="${text}NODE_ID={{.Node.ID}}\n"
+	text="${text}TASK_ID={{.Task.ID}}\n"
+	text="${text}SERVICE_ID={{.Service.ID}}\n"
+	text="${text}CLOUD_OS_GATEWAY=cloud_os_standard_1\n"
+	text="${text}CLOUD_OS_KEY=${CLOUD_OS_KEY}\n"
+	text="${text}SSH_USER=${SSH_USER}\n"
+	text="${text}SSH_PASSWORD=${SSH_PASSWORD}\n"
+	echo -e $text | tee $ENV_CONFIG_PATH > /dev/null
 }
 
 function print_env_config()
@@ -77,38 +78,48 @@ function download_container()
 
 function create_network()
 {
-	res=`docker network ls | grep cloud_network`
+	local res=`sudo docker network ls | grep cloud_network 2>/dev/null`
 	if [ -z "$res" ]; then
 		echo "Create docker cloud network"
-		docker network create --subnet 172.21.0.0/16 --driver=overlay \
+		sudo docker network create --subnet 172.21.0.0/16 --driver=overlay \
 			--attachable cloud_network -o "com.docker.network.bridge.name"="cloud_network"
+		if [ $? -ne 0 ]; then
+			echo "Failed to create docker network"
+			exit 1
+		fi
 	fi
 }
 
 function compose()
 {
 	echo "Compose cloud os"
-	res=`docker ps -a |grep cloud_os_standard`
+	local res=`sudo docker ps -a |grep cloud_os_standard`
 	if [ ! -z "$res" ]; then
-		docker stop cloud_os_standard > /dev/null
-		docker rm cloud_os_standard > /dev/null
+		sudo docker stop cloud_os_standard > /dev/null
+		sudo docker rm cloud_os_standard > /dev/null
 	fi
-	docker run -d \
+	sudo docker run -d \
 		-p 8022:22 \
 		-p 8080:80 \
 		-v cloud_os_standard:/data \
-		-v /home/ubuntu/bayrell/cloud_os_standard_0.5.1/src:/srv \
 		-v /var/run/docker.sock:/var/run/docker.sock:ro \
 		-v /etc/hostname:/etc/hostname_orig:ro \
+		-v $SCRIPT_PATH/src:/srv \
 		-e WWW_UID=1000 \
 		-e WWW_GID=1000 \
 		--name cloud_os_standard \
 		--hostname cloud_os_standard.local \
-		--env-file $SCRIPT_PATH/example/env.conf \
+		--env-file $ENV_CONFIG_PATH \
 		--restart unless-stopped \
 		--network cloud_network \
 		bayrell/cloud_os_standard:$VERSION
+	if [ $? -ne 0 ]; then
+		echo "Failed to compose cloud os"
+		exit 1
+	fi
 }
+
+read_env_config
 
 case "$1" in
 	
@@ -130,10 +141,6 @@ case "$1" in
 	compose)
 		generate_env_config
 		compose
-	;;
-	
-	compose_test)
-		compose_test
 	;;
 	
 	print)
